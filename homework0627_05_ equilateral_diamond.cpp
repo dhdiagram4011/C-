6a2fc00b3e4bf01c02321142106249e1cca0e0cb
kim do hyoung <dhkim2@aspringcloud.com>
2022-06-26 15:46:16 +0900

    'diamond'

--- homework0627_05_ equilateral_diamond.cpp
+++ homework0627_05_ equilateral_diamond.cpp
@@ -10,11 +10,11 @@ int main()
 
     for(int i=0; i<num;i++)
     {
-        for (int j=num-1;j>i;j--)
+        for (int j=num-1;j>i;j--) //다이아몬드 아래부분
         {
             cout << " ";
         }
-        for(int j=0;j<2*i+1;j++)
+        for(int j=0;j<2*i+1;j++) //1,3,5,7,9.. 2차이로 다이아몬드 쌓기,
         {
             cout << "*";
         }
@@ -26,7 +26,7 @@ int main()
         {
             cout << " ";
         }
-        for (int j=2*num-1;j>2*i;j--)
+        for (int j=2*num-1;j>2*i;j--) //9,7,5,3,1  단위로 감소,, 다이아몬드 아랫부분
         {
             cout << "*";
         }
